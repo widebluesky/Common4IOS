769539be77de7be3239b04e42201c85441aee153
widebluesky <widebluesky@qq.com>
2012-12-11 21:38:48 +0800

    增加添加下划线方法。

--- common4ios/Common4IOS/SuperClass/View/HP_UILabel.h
+++ common4ios/Common4IOS/SuperClass/View/HP_UILabel.h
@@ -10,10 +10,14 @@
 
 @interface HP_UILabel : UILabel
 
+@property(nonatomic) BOOL hasUnderline;
+@property(nonatomic) UIColor *underLineColor ;
 @property(nonatomic) UIEdgeInsets insets;
 
+//根据内边距初始化UILabel
 - (id)initWithFrame:(CGRect)frame andInsets:(UIEdgeInsets) insets;
 
+//根据内边距初始化UILabel
 - (id)initWithInsets:(UIEdgeInsets) insets;
 
 @end
--- common4ios/Common4IOS/SuperClass/View/HP_UILabel.m
+++ common4ios/Common4IOS/SuperClass/View/HP_UILabel.m
@@ -10,8 +10,19 @@
 
 @implementation HP_UILabel
 
+@synthesize hasUnderline;
+@synthesize underLineColor;
+
 @synthesize insets = _insets;
 
+-(id) init{
+    self = [super init];
+    if (self) {
+        hasUnderline = NO;
+        underLineColor = [UIColor blackColor];
+    }
+    return self;
+}
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -37,8 +48,49 @@
     return self;
 }
 
+
 - (void)drawTextInRect:(CGRect)rect {
     return [super drawTextInRect:UIEdgeInsetsInsetRect(rect, self.insets)];
 }
 
+
+-(void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    if (hasUnderline) {
+        CGContextRef ctx = UIGraphicsGetCurrentContext();
+        CGSize fontSize =[self.text sizeWithFont:self.font
+                                        forWidth:self.bounds.size.width
+                                   lineBreakMode:UILineBreakModeTailTruncation];
+        // Get the fonts color.
+        CGColorRef colorRef = [underLineColor CGColor];
+        int numComponents = CGColorGetNumberOfComponents(colorRef);
+        if (numComponents >= 3){
+            const CGFloat *rgbComponents = CGColorGetComponents(colorRef);
+            CGContextSetRGBFillColor(ctx, rgbComponents[0], rgbComponents[1], rgbComponents[2], rgbComponents[3]);
+        }
+        
+        // Line Width : make thinner or bigger if you want
+        CGContextSetLineWidth(ctx, 1.0f);
+        
+        // Calculate the starting point (left) and target (right)
+        CGPoint l = CGPointMake(self.frame.size.width/2.0 - fontSize.width/2.0,
+                                self.frame.size.height/2.0 +fontSize.height/2.0);
+        CGPoint r = CGPointMake(self.frame.size.width/2.0 + fontSize.width/2.0,
+                                self.frame.size.height/2.0 + fontSize.height/2.0);
+        
+        
+        // Add Move Command to point the draw cursor to the starting point
+        CGContextMoveToPoint(ctx, l.x, l.y);
+        
+        // Add Command to draw a Line
+        CGContextAddLineToPoint(ctx, r.x, r.y);
+        
+        
+        // Actually draw the line.
+        CGContextStrokePath(ctx);
+        
+        // should be nothing, but who knows...
+    }
+}
 @end
